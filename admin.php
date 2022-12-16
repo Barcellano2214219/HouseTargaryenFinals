5531d342d902333cd27b315f064daefd007a0eff
Dave14 <Dave14@gmail.com>
2022-12-16 12:59:31 +0900

    LoginUpdates

--- admin.php
+++ admin.php
@@ -1,74 +1,64 @@
-
 <!DOCTYPE html>
 <html>
-	<head>
-		<?php include('header.php') ?>
-        <?php 
-        session_start();
-        if(isset($_SESSION['login_id'])){
-            header('Location:home.php');
-        }
-        ?>
-		<title>Admin | Simple Online Quiz System</title>
-	</head>
 
-	<body id='login-body' class="bg-light">
+<head>
+    <?php include('header.php') ?>
+    <?php
+    session_start();
+    if (isset($_SESSION['login_id'])) {
+        header('Location:home.php');
+    }
+    ?>
+    <title>Admin | Simple Online Quiz System</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-        <div class="card col-md-6 offset-md-3 text-center bg-primary mb-4">
-            <h3 class="he3-responsive text-white">Simple Online Quiz System</h3>
+<body>
+    <form id="login-frm">
+        <div class="form-group">
+            <h2>Login</h2>
+            <label>UserName</label>
+            <input type="text" name="username" placeholder="Username" class="form-control">
         </div>
-		<div class="card col-md-4 offset-md-4 mt-4">
-                <div class="card-header-edge text-white">
-                    <strong>Login</strong>
-                </div>
-            <div class="card-body">
-                     <form id="login-frm">
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input type="username" name="username" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" name="password" class="form-control">
-                        </div> 
-                        <div class="form-group text-right">
-                            <button class="btn btn-primary btn-block" name="submit">Login</button>
-                        </div>
-                        
-                    </form>
-            </div>
+        <div class="form-group">
+            <label>UserName</label>
+            <input type="password" name="password" placeholder="Password" class="form-control">
         </div>
+        <div class="form-group">
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</body>
+<script>
+    $(document).ready(function() {
+        $('#login-frm').submit(function(e) {
+            e.preventDefault()
+            $('#login-frm button').attr('disable', true)
+            $('#login-frm button').html('Please wait...')
 
-		</body>
-
-        <script>
-            $(document).ready(function(){
-                $('#login-frm').submit(function(e){
-                    e.preventDefault()
-                    $('#login-frm button').attr('disable',true)
-                    $('#login-frm button').html('Please wait...')
-
-                    $.ajax({
-                        url:'./login_auth.php?type=1',
-                        method:'POST',
-                        data:$(this).serialize(),
-                        error:err=>{
-                            console.log(err)
-                            alert('An error occured');
-                            $('#login-frm button').removeAttr('disable')
-                            $('#login-frm button').html('Login')
-                        },
-                        success:function(resp){
-                            if(resp == 1){
-                                location.replace('home.php')
-                            }else{
-                                alert("Incorrect username or password.")
-                                $('#login-frm button').removeAttr('disable')
-                                $('#login-frm button').html('Login')
-                            }
-                        }
-                    })
-
-                })
+            $.ajax({
+                url: './login_auth.php?type=1',
+                method: 'POST',
+                data: $(this).serialize(),
+                error: err => {
+                    console.log(err)
+                    alert('An error occured');
+                    $('#login-frm button').removeAttr('disable')
+                    $('#login-frm button').html('Login')
+                },
+                success: function(resp) {
+                    if (resp == 1) {
+                        location.replace('home.php')
+                    } else {
+                        alert("Incorrect username or password.")
+                        $('#login-frm button').removeAttr('disable')
+                        $('#login-frm button').html('Login')
+                    }
+                }
             })
-        </script>
+
+        })
+    })
+</script>
+
 </html>
